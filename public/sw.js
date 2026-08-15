4705a49e5fb2b9b737feb3f75b50a245e6f38b75
lucasfelipe131 <lucasfelipe.oliveira@hotmail.com>
2026-08-15 14:15:26 -0300

    Add public/sw.js

---
+++ public/sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'fluent8-v3';
+const CORE = ['/', '/styles.css', '/app.js', '/manifest.webmanifest', '/icon.svg'];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(Promise.all([
+    self.clients.claim(),
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+  ]));
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
